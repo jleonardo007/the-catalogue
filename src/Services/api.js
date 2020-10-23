5c02d51a92508e2f89fe07f2da47969d56480a92
Leonardo Bravo <j.leonardo.bravo@gmail.com>
2020-10-22 23:44:17 -0400

    search and requests by categories added

--- src/Services/api.js
+++ src/Services/api.js
@@ -7,6 +7,38 @@ export default {
     return movies.results;
   },
 
+  fetchPopularMovies: async () => {
+    const response = await fetch(
+      `https://api.themoviedb.org/3/movie/popular?api_key=af058d99e581a98b7f284b08a4b31810&language=en-US&page=1`
+    );
+    const movies = await response.json();
+    return movies.results;
+  },
+
+  fetchTrendingMovies: async () => {
+    const response = await fetch(
+      `https://api.themoviedb.org/3/trending/movie/day?api_key=af058d99e581a98b7f284b08a4b31810`
+    );
+    const movies = await response.json();
+    return movies.results;
+  },
+
+  fetchUpcomingMovies: async () => {
+    const response = await fetch(
+      `https://api.themoviedb.org/3/movie/upcoming?api_key=af058d99e581a98b7f284b08a4b31810&language=en-US&page=1`
+    );
+    const movies = await response.json();
+    return movies.results;
+  },
+
+  searchMovie: async (query) => {
+    const response = await fetch(
+      `https://api.themoviedb.org/3/search/movie?api_key=af058d99e581a98b7f284b08a4b31810&language=en-US&query=${query}&page=1&include_adult=false`
+    );
+    const search = await response.json();
+    return search.results;
+  },
+
   fetchMovie: async (movieID) => {
     const response = await fetch(
       `https://api.themoviedb.org/3/movie/${movieID}?api_key=af058d99e581a98b7f284b08a4b31810&language=en-US`
