cab30b339a211f670cb0f0d6d7f3ca95838d3571
Leonardo Bravo <j.leonardo.bravo@gmail.com>
2020-10-30 01:05:13 -0400

    api's requests refactorized to make "infinite scroll" in home component

--- src/Services/api.js
+++ src/Services/api.js
@@ -1,42 +1,46 @@
 export default {
-  fetchNowPlayingMovies: async () => {
+  fetchNowPlayingMovies: async (pageNumber = 1) => {
     const response = await fetch(
-      `https://api.themoviedb.org/3/movie/now_playing?api_key=af058d99e581a98b7f284b08a4b31810&language=en-US&page=1`
+      `https://api.themoviedb.org/3/movie/now_playing?api_key=af058d99e581a98b7f284b08a4b31810&language=en-US&page=${pageNumber}`
     );
-    const movies = await response.json();
-    return movies.results;
+    const { results } = await response.json();
+    return results;
   },
 
-  fetchPopularMovies: async () => {
+  fetchPopularMovies: async (pageNumber = 1) => {
     const response = await fetch(
-      `https://api.themoviedb.org/3/movie/popular?api_key=af058d99e581a98b7f284b08a4b31810&language=en-US&page=1`
+      `https://api.themoviedb.org/3/movie/popular?api_key=af058d99e581a98b7f284b08a4b31810&language=en-US&page=${pageNumber}`
     );
-    const movies = await response.json();
-    return movies.results;
+
+    const { results } = await response.json();
+    return results;
   },
 
   fetchTrendingMovies: async () => {
     const response = await fetch(
       `https://api.themoviedb.org/3/trending/movie/day?api_key=af058d99e581a98b7f284b08a4b31810`
     );
-    const movies = await response.json();
-    return movies.results;
+
+    const { results } = await response.json();
+    return results;
   },
 
-  fetchUpcomingMovies: async () => {
+  fetchUpcomingMovies: async (pageNumber = 1) => {
     const response = await fetch(
-      `https://api.themoviedb.org/3/movie/upcoming?api_key=af058d99e581a98b7f284b08a4b31810&language=en-US&page=1`
+      `https://api.themoviedb.org/3/movie/upcoming?api_key=af058d99e581a98b7f284b08a4b31810&language=en-US&page=${pageNumber}`
     );
-    const movies = await response.json();
-    return movies.results;
+
+    const { results } = await response.json();
+    return results;
   },
 
-  searchMovie: async (query) => {
+  searchMovie: async (query, pageNumber = 1) => {
     const response = await fetch(
-      `https://api.themoviedb.org/3/search/movie?api_key=af058d99e581a98b7f284b08a4b31810&language=en-US&query=${query}&page=1&include_adult=false`
+      `https://api.themoviedb.org/3/search/movie?api_key=af058d99e581a98b7f284b08a4b31810&language=en-US&query=${query}&page=${pageNumber}&include_adult=false`
     );
-    const search = await response.json();
-    return search.results;
+
+    const { results } = await response.json();
+    return results;
   },
 
   fetchMovie: async (movieID) => {
@@ -51,6 +55,7 @@ export default {
     const response = await fetch(
       `https://api.themoviedb.org/3/movie/${movieID}/videos?api_key=af058d99e581a98b7f284b08a4b31810&language=en-US`
     );
+
     const videos = await response.json();
     return videos.results.find((video) => video.type === "Trailer");
   },
@@ -59,6 +64,7 @@ export default {
     const response = await fetch(
       `https://api.themoviedb.org/3/movie/${movieID}/similar?api_key=af058d99e581a98b7f284b08a4b31810&language=en-US&page=1`
     );
+
     const similarMovies = await response.json();
     return similarMovies.results.slice(0, 10);
   },
