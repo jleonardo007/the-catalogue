41d367215bfa1759d089db2e887031f6b46d19cd
Leonardo Bravo <j.leonardo.bravo@gmail.com>
2020-10-30 01:06:00 -0400

    "Infinite scroll" feature added

--- src/Components/Home/Home.js
+++ src/Components/Home/Home.js
@@ -1,53 +1,114 @@
 import React from "react";
-import { useState, useEffect } from "react";
+import { useState, useEffect, useRef } from "react";
 import FeaturesBar from "../FeaturesBar/FeaturesBar";
 import MovieCard from "../MovieCard/MovieCard";
 import API from "../../Services/api";
 import "./Home.css";
 
 function Home() {
-  const [currentMovies, setCurrent] = useState(null);
-  const [title, setTitle] = useState("Recent Movies");
+  const observerTarget = useRef(null);
+  const [title, setTitle] = useState("Now Playing");
+  const [movies, setMovies] = useState(null);
+  const [pageNumber, setPage] = useState(1);
+  const [query, setQuery] = useState("");
 
   useEffect(() => {
     (async () => {
-      setCurrent(await API.fetchNowPlayingMovies());
+      setMovies(await API.fetchNowPlayingMovies());
     })();
   }, []);
 
+  useEffect(() => {
+    const handleInfiniteScroll = (entries) => {
+      if (entries[0].isIntersecting) {
+        setPage((prevPage) => prevPage + 1);
+      }
+    };
+
+    const observer = new IntersectionObserver(handleInfiniteScroll, {
+      root: null,
+      rootMargin: "20px",
+      thresold: 1.0,
+    });
+
+    if (observerTarget.current) observer.observe(observerTarget.current);
+  }, []);
+
+  useEffect(() => {
+    const loadMoreMovies = async () => {
+      let results = [];
+      switch (title) {
+        case "Popular Movies":
+          results = await API.fetchPopularMovies(pageNumber);
+          break;
+
+        case "Upcoming":
+          results = await API.fetchUpcomingMovies(pageNumber);
+          break;
+
+        case "Now Playing":
+          results = await API.fetchNowPlayingMovies(pageNumber);
+          break;
+
+        default:
+          break;
+      }
+      setMovies((prevMovies) =>
+        prevMovies ? prevMovies.concat(...results) : null
+      );
+    };
+
+    if (pageNumber > 1) loadMoreMovies();
+  }, [pageNumber, title]);
+
+  useEffect(() => {
+    const loadMoreMoviesOnSearch = async () => {
+      const results = await API.searchMovie(query, pageNumber);
+      setMovies((prevMovies) =>
+        prevMovies ? prevMovies.concat(...results) : null
+      );
+    };
+    if (pageNumber > 1 && query) loadMoreMoviesOnSearch();
+  }, [pageNumber, query]);
+
   const fetchMovies = async (movies) => {
-    setCurrent(null);
+    setQuery("");
+    setPage(1);
+    setMovies(null);
+
     switch (movies) {
       case "trending":
         setTitle("Trending Movies");
-        setCurrent(await API.fetchTrendingMovies());
+        setMovies(await API.fetchTrendingMovies());
         break;
 
       case "popular":
         setTitle("Popular Movies");
-        setCurrent(await API.fetchPopularMovies());
+        setMovies(await API.fetchPopularMovies());
         break;
 
       case "upcoming":
         setTitle("Upcoming");
-        setCurrent(await API.fetchUpcomingMovies());
+        setMovies(await API.fetchUpcomingMovies());
         break;
 
       default:
-        setTitle("Recent Movies");
-        setCurrent(await API.fetchNowPlayingMovies());
+        setTitle("Now Playing");
+        setMovies(await API.fetchNowPlayingMovies());
         break;
     }
   };
 
   const searchMovies = async (query) => {
+    setQuery(query);
     const result = await API.searchMovie(query);
+    setMovies(null);
 
     if (result.length === 0) setTitle("No results");
     else {
-      setCurrent(null);
+      setPage(1);
       setTitle(`Results for "${query}"`);
-      setCurrent(await API.searchMovie(query));
+      setMovies(await API.searchMovie(query));
     }
   };
 
@@ -73,15 +134,16 @@ function Home() {
       />
       <h1 className="home__title">{title}</h1>
 
-      {currentMovies ? (
+      {movies ? (
         <div className="home__movies">
-          {currentMovies.map((movie, index) => {
+          {movies.map((movie, index) => {
             return <MovieCard key={index} movie={movie} />;
           })}
         </div>
       ) : (
         <div className="fetching-movie">Fetching movies</div>
       )}
+      <div className="home__loader" ref={observerTarget} />
     </div>
   );
 }
