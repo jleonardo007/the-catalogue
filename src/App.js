0f1b19ba3d3f5a0e164db1308e83a076086f3141
Leonardo Bravo <j.leonardo.bravo@gmail.com>
2020-11-01 00:21:46 -0400

    Routing error fixed

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ import Movie from "./Components/Movie/Movie";
 
 function App() {
   return (
-    <Router basename={process.env.PUBLIC_URL || ""}>
+    <Router basename="/">
       <Switch>
         <Route path="/movie/:id">
           <Movie />
